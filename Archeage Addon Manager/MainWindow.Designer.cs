3ee23aeea9011353037b8b54a3f5a85404472ecd
DESKTOP-2PSOO5E\Nidoran <seanm07@googlemail.com>
2024-01-17 00:44:22 +0000

    Initial Commit

---
+++ Archeage Addon Manager/MainWindow.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Archeage_Addon_Manager
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageAddonSourcesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.propertiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.creditsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.developersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uploadAddonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(395, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Install Selected";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(514, 297);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Window;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.progressBar1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 318);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(514, 63);
+            this.panel2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0 addons selected";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 38);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(377, 13);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.developersToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(514, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manageAddonSourcesToolStripMenuItem,
+            this.propertiesToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // manageAddonSourcesToolStripMenuItem
+            // 
+            this.manageAddonSourcesToolStripMenuItem.Name = "manageAddonSourcesToolStripMenuItem";
+            this.manageAddonSourcesToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.manageAddonSourcesToolStripMenuItem.Text = "Check For Updates";
+            // 
+            // propertiesToolStripMenuItem
+            // 
+            this.propertiesToolStripMenuItem.Name = "propertiesToolStripMenuItem";
+            this.propertiesToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.propertiesToolStripMenuItem.Text = "Properties";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.creditsToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // creditsToolStripMenuItem
+            // 
+            this.creditsToolStripMenuItem.Name = "creditsToolStripMenuItem";
+            this.creditsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.creditsToolStripMenuItem.Text = "About";
+            this.creditsToolStripMenuItem.Click += new System.EventHandler(this.ShowAboutDialog);
+            // 
+            // developersToolStripMenuItem
+            // 
+            this.developersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uploadAddonToolStripMenuItem});
+            this.developersToolStripMenuItem.Name = "developersToolStripMenuItem";
+            this.developersToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.developersToolStripMenuItem.Text = "Developers";
+            // 
+            // uploadAddonToolStripMenuItem
+            // 
+            this.uploadAddonToolStripMenuItem.Name = "uploadAddonToolStripMenuItem";
+            this.uploadAddonToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.uploadAddonToolStripMenuItem.Text = "Upload Addon";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 381);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(530, 420);
+            this.MinimumSize = new System.Drawing.Size(530, 420);
+            this.Name = "MainWindow";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Archeage Addon Manager";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageAddonSourcesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem propertiesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem creditsToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ToolStripMenuItem developersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uploadAddonToolStripMenuItem;
+    }
+}
+
---
+++ Archeage Addon Manager/MainWindow.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Archeage_Addon_Manager {
+    public partial class MainWindow : Form {
+        public class AddonData {
+            public string name { get; set; }
+            public string description { get; set; }
+            public string author { get; set; }
+            public float version { get; set; }
+            public string dataPath { get; set; }
+        }
+
+        public class AddonWidget {
+            public CheckBox checkbox { get; set; }
+            public Label titleLabel { get; set; }
+            public Label descriptionLabel { get; set; }
+        }
+
+        List<AddonData> addons = new List<AddonData>();
+        List<AddonWidget> addonWidgets = new List<AddonWidget>();
+
+        public MainWindow() {
+            InitializeComponent();
+
+            AddAddonsFromURL("https://www.spacemeat.space/aamods/data.json");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            
+
+            for (int i = 0; i < addonWidgets.Count; i++) {
+                if (addonWidgets[i].checkbox.Checked) {
+                    Console.WriteLine("Checked: " + addonWidgets[i].titleLabel.Text);
+                }
+            }
+        }
+
+        public void OnClickAddonWidget(int addonIndex) {
+            addonWidgets[addonIndex].checkbox.Checked = !addonWidgets[addonIndex].checkbox.Checked;
+
+            OnCheckboxesUpdated();
+        }
+
+        public void OnCheckboxesUpdated() {
+            int totalChecked = 0;
+
+            for (int i = 0; i < addonWidgets.Count; i++) {
+                if (addonWidgets[i].checkbox.Checked) {
+                    totalChecked++;
+                }
+            }
+
+            label1.Text = totalChecked + " addon" + (totalChecked > 1 ? "s" : "") + " selected";
+        }
+
+        public void AddAddon(AddonData addonData) {
+            // Store the addon data in a list for later referencing
+            addons.Add(addonData);
+
+            // Visually display the addon in the addon list panel
+            // Create a horizontal group to contain each addon
+            FlowLayoutPanel horizontalGroup = new FlowLayoutPanel() {
+                Dock = DockStyle.Top,
+                Width = panel1.Width - SystemInformation.VerticalScrollBarWidth,
+                AutoSize = true,
+                BackColor = Color.LightGray
+            };
+
+            // Container for the checkbox so we can center it
+            FlowLayoutPanel checkboxGroup = new FlowLayoutPanel() {
+                Dock = DockStyle.Left,
+                Width = 30,
+                Margin = new Padding(0, 0, 0, 0)
+            };
+
+            // Create a checkbox to select the addon for installation or to show already installed
+            CheckBox addonCheckbox = new CheckBox() {
+                Width = 15,
+                AutoCheck = false
+            };
+
+            // Add the checkbox to the checkboxGroup group
+            checkboxGroup.Controls.Add(addonCheckbox);
+
+            // Group to contain right side text labels
+            FlowLayoutPanel textGroup = new FlowLayoutPanel() {
+                Width = horizontalGroup.Width - checkboxGroup.Width,
+                Height = 43 + ((addonData.description.Split(new string[] { "\n" }, StringSplitOptions.None).Length - 1) * 15), // Set height based on newlines in description
+                Margin = new Padding(0, 0, 0, 0)
+            };
+
+            // Title label which contains addon name, version and author
+            Label titleLabel = new Label() {
+                Width = textGroup.Width,
+                Font = new Font("Microsoft Sans Serif", 10, FontStyle.Bold),
+                Text = addonData.name + " - v" + addonData.version + " by " + addonData.author,
+                Margin = new Padding(0, 0, 0, 0)
+            };
+
+            // Description label displayed below the title label
+            Label descriptionLabel = new Label() {
+                Width = textGroup.Width,
+                AutoSize = true,
+                Text = addonData.description,
+                Margin = new Padding(0, 0, 0, 0)
+            };
+
+            // Add the title and description labels to the right side text group
+            textGroup.Controls.Add(titleLabel);
+            textGroup.Controls.Add(descriptionLabel);
+
+            // Add the left side checkboxGroup and right side textGroup to the horizontal group
+            horizontalGroup.Controls.Add(checkboxGroup);
+            horizontalGroup.Controls.Add(textGroup);
+
+            // Create a spacer to separate each addon with a visual spacer
+            FlowLayoutPanel spacerPanel = new FlowLayoutPanel(){
+                Dock = DockStyle.Top,
+                Width = panel1.Width - SystemInformation.VerticalScrollBarWidth,
+                Height = 1,
+                BackColor = Color.White
+            };
+
+            // Controls added to the panel are added bottom to top based on the top dock style
+            // Add the spacer to the panel
+            panel1.Controls.Add(spacerPanel);
+
+            // Add the horizontal group to the panel
+            panel1.Controls.Add(horizontalGroup);
+
+            // Vertically center the checkbox
+            int verticalMargin = (checkboxGroup.Height - addonCheckbox.Height) / 2;
+            int horizontalMargin = (checkboxGroup.Width - addonCheckbox.Width) / 2;
+            addonCheckbox.Margin = new Padding(horizontalMargin, verticalMargin, horizontalMargin, verticalMargin);
+
+            int curId = addonWidgets.Count;
+
+            horizontalGroup.Click += (sender, e) => OnClickAddonWidget(curId);
+            checkboxGroup.Click += (sender, e) => OnClickAddonWidget(curId);
+            addonCheckbox.Click += (sender, e) => OnClickAddonWidget(curId);
+            textGroup.Click += (sender, e) => OnClickAddonWidget(curId);
+            titleLabel.Click += (sender, e) => OnClickAddonWidget(curId);
+            descriptionLabel.Click += (sender, e) => OnClickAddonWidget(curId);
+
+            // Cache the addonWidgets in a list for easier referencing
+            addonWidgets.Add(new AddonWidget() {
+                checkbox = addonCheckbox,
+                titleLabel = titleLabel,
+                descriptionLabel = descriptionLabel
+            });
+        }
+
+        // Load addons from a URL containing a JSON array of AddonData objects
+        public void AddAddonsFromURL(string url) {
+            // Send a web request to the specified URL which should return a JSON array of AddonData objects
+            string jsonInput = new System.Net.WebClient().DownloadString(url);
+
+            try {
+                // Deserialize the JSON array into a list of AddonData objects
+                List<AddonData> addonList = JsonConvert.DeserializeObject<List<AddonData>>(jsonInput);
+
+                // Iterate through each addon and call AddAddon
+                foreach (AddonData addon in addonList) {
+                    AddAddon(addon);
+                }
+            } catch (Exception e) {
+                Console.WriteLine("Error parsing JSON result from URL " + url + " - " + e.Message);
+            }
+        }
+
+        public void ShowAboutDialog(object sender, EventArgs e) {
+            MessageBox.Show("Not endorsed by XLGames or Kakao Games, we don't reflect views or opinion of anyone officially involved in Archeage.\n\nKakao strongly does not recommend addon usage, you accept the potential game breaking risks!\n\nArcheage Addon Manager created by Nidoran\n\nBig thanks to Ingram for all his work and help to make this possible and his work on AAPatcher.\n\nAdditional thanks to Tamaki & Strawberry", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
