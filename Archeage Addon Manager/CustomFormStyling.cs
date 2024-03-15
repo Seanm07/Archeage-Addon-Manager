1ad9abb038c431a733a33733782ccd3c95b35bf5
Nidoran <seanm07@googlemail.com>
2024-03-15 01:20:59 +0000

    Even more work on scrollbar smoothing and rendering fixes

--- Archeage Addon Manager/CustomFormStyling.cs
+++ Archeage Addon Manager/CustomFormStyling.cs
@@ -112,7 +112,7 @@ namespace Archeage_Addon_Manager {
         private static extern int ShowScrollBar(IntPtr hWnd, int wBar, int bShow);
 
         private int thumbMargin = 4;
-        private int scrollPosition = 0; // Current scroll position
+        private float scrollPosition = 0f;
         private bool thumbDragging = false;
         private int thumbDragOffset = 0;
 
@@ -128,74 +128,50 @@ namespace Archeage_Addon_Manager {
         }
 
         public CustomPanel() {
-            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint, true);
 
-            this.Layout += LayoutReady;
+            this.Layout += LayoutUpdate;
         }
 
         // Called once the layout is ready and heights are no longer 0
-        private void LayoutReady(object? sender, LayoutEventArgs e) {
-            RefreshLayout();
+        private void LayoutUpdate(object? sender, LayoutEventArgs e) {
+            panelContentsHeight = ContentHeight();
 
-            AutoScroll = false;
-            VerticalScroll.Enabled = false;
+            scrollBgWidth = SystemInformation.VerticalScrollBarWidth;
+            scrollBgHeight = Height;
+            scrollBgX = Width - scrollBgWidth;
+            scrollBgY = 0;
+
+            thumbWidth = scrollBgWidth - (thumbMargin * 2);
+            thumbHeight = (int)((float)scrollBgHeight * ((float)scrollBgHeight / (float)panelContentsHeight));
+
+            // This needs to be called again when releasing the scrollbar otherwise the windows scrollbar blocks the custom scrollbar again
+            _ = ShowScrollBar(Handle, 1, 0); // ID 1 is vertical scrollbar
+
+            AutoScroll = true;
             VerticalScroll.Visible = false;
         }
 
-        private void RefreshLayout() {
-            panelContentsHeight = ContentHeight();
-
-            if (panelContentsHeight > 0) {
-                scrollBgWidth = SystemInformation.VerticalScrollBarWidth;
-                scrollBgHeight = Height;
-                scrollBgX = Width - scrollBgWidth;
-                scrollBgY = 0;
-
-                thumbWidth = scrollBgWidth - (thumbMargin * 2);
-                thumbHeight = (int)((float)scrollBgHeight * ((float)scrollBgHeight / (float)panelContentsHeight));
-
-                PerformScroll();
-            }
-        }
-
         protected override void OnSizeChanged(EventArgs e) {
-            PerformScroll();
-        }
+            // This needs to be called again when releasing the scrollbar otherwise the windows scrollbar blocks the custom scrollbar again
+            _ = ShowScrollBar(Handle, 1, 0); // ID 1 is vertical scrollbar
 
-        private int lastScrollValue = -1;
+            AutoScroll = false;
+        }
 
         private void PerformScroll() {
             thumbHeight = (int)((float)scrollBgHeight * ((float)scrollBgHeight / (float)panelContentsHeight));
             thumbPos = (int)((float)scrollPosition * ((float)scrollBgHeight - (float)thumbHeight) / ((float)panelContentsHeight - (float)scrollBgHeight));
 
             // Calculate scroll percentage
-            float scrollPercentage = (float)scrollPosition / ((float)panelContentsHeight - (float)Height);
+            float scrollPercentage = (float)scrollPosition / ((float)panelContentsHeight - (float)thumbHeight);
+            int scrollY = (int)(scrollPercentage * VerticalScroll.Maximum);
 
-            if(VerticalScroll.Maximum != Height)
-                VerticalScroll.Maximum = Height;
-
-            int scrollValue = (int)(scrollPercentage * VerticalScroll.Maximum);
-
-            if (scrollValue != lastScrollValue || VerticalScroll.Value != scrollValue) {
-                lastScrollValue = scrollValue;
-
-                
-                VerticalScroll.Value = scrollValue;
-            }
-
-            // This needs to be called again when releasing the scrollbar otherwise the windows scrollbar blocks the custom scrollbar again
-            _ = ShowScrollBar(Handle, 1, 0); // ID 1 is vertical scrollbar
-
-            // Workaround for small scrolls not updating the child panels even when invalidating or refreshing
-            OnVisibleChanged(EventArgs.Empty);
-
-            AutoScroll = false;
-            VerticalScroll.Visible = false;
+            AutoScrollPosition = new Point(0, scrollY);
+            VerticalScroll.Value = scrollY;
         }
 
         protected override void OnPaint(PaintEventArgs e) {
-            base.OnPaint(e);
-
             if (panelContentsHeight > thumbHeight) {
                 // Draw the scrollbar background
                 using (var bgBrush = new SolidBrush(Color.FromArgb(255, 33, 35, 38)))
@@ -207,28 +183,23 @@ namespace Archeage_Addon_Manager {
             }
         }
 
-        private int lastScroll = -1;
-
         protected override void OnMouseWheel(MouseEventArgs e) {
-            // Don't run the base mouse wheel event otherwise it can glitch making the windows default scroll reappear
-            //base.OnMouseWheel(e);
+            base.OnMouseWheel(e);
+            
+            float scrollChange = e.Delta * 0.5f;
 
-            scrollPosition = Math.Max(0, Math.Min(scrollPosition - e.Delta, panelContentsHeight - scrollBgHeight));
+            scrollPosition = Math.Max(0f, Math.Min(scrollPosition - scrollChange, panelContentsHeight - scrollBgHeight));
 
-            if (scrollPosition != lastScroll) {
-                lastScroll = scrollPosition;
-
-                // Force a repaint
-                PerformScroll();
-            }
+            // Force a repaint
+            PerformScroll();
         }
 
         protected override void OnMouseDown(MouseEventArgs e) {
+            base.OnMouseDown(e);
+
             if (e.Button == MouseButtons.Left) {
                 // Get the scroll bounds of the thumb and store it in a rectangle
                 Rectangle thumbBounds = new Rectangle(scrollBgX, thumbPos, scrollBgWidth, thumbHeight);
-
-                Debug.WriteLine(thumbBounds);
 
                 // Check if the mouse is within the thumb bounds
                 if (thumbBounds.Contains(e.Location)) {
@@ -242,6 +213,8 @@ namespace Archeage_Addon_Manager {
         }
 
         protected override void OnMouseUp(MouseEventArgs e) {
+            base.OnMouseUp(e);
+
             if (e.Button == MouseButtons.Left) {
                 thumbDragging = false;
 
@@ -249,22 +222,15 @@ namespace Archeage_Addon_Manager {
             }
         }
 
-        private int lastScrollPositionSync = 0;
-
         protected override void OnMouseMove(MouseEventArgs e) {
-            //base.OnMouseMove(e);
+            base.OnMouseMove(e);
 
             if (thumbDragging) {
                 // Calculate new scroll position based on thumb drag
-                scrollPosition = Math.Max(0, Math.Min((int)(((float)(e.Y - thumbDragOffset) / (float)(scrollBgHeight - thumbHeight)) * (float)(panelContentsHeight - scrollBgHeight)), panelContentsHeight - scrollBgHeight));
+                scrollPosition = Math.Max(0f, Math.Min((((float)(e.Y - thumbDragOffset) / (float)(scrollBgHeight - thumbHeight)) * (float)(panelContentsHeight - scrollBgHeight)), panelContentsHeight - scrollBgHeight));
 
-                // If the difference between scrollPosition and lastScrollPositionSync is greater than 20 or we hit the top/bottom, sync the scroll position
-                if (lastScrollPositionSync != scrollPosition && Math.Abs(scrollPosition - lastScrollPositionSync) > 20 || (scrollPosition == 0 && lastScrollPositionSync != 0) || (scrollPosition == panelContentsHeight - scrollBgHeight && lastScrollPositionSync != panelContentsHeight - scrollBgHeight)) {
-                    lastScrollPositionSync = scrollPosition;
-
-                    // Force a repaint
-                    PerformScroll();
-                }
+                // Force a repaint
+                PerformScroll();
             }
         }
 
